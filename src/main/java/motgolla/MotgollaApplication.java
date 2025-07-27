7009e8d7f349bd87e22ff227d85424ea54103883
taeyeon0319 <xodus1349@likelion.org>
2025-07-27 21:11:45 +0900

    feat: 알림 FCM 전송

--- src/main/java/motgolla/MotgollaApplication.java
+++ src/main/java/motgolla/MotgollaApplication.java
@@ -2,8 +2,10 @@ package motgolla;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MotgollaApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/motgolla/domain/notification/mapper/NotificationMapper.java
@@ -0,0 +1,14 @@
+package motgolla.domain.notification.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface NotificationMapper {
+    void insertToken(@Param("memberId") Long memberId, @Param("token") String token);
+    List<Long> findUserIdsWhoRegisteredToday();
+    String findTokenByUserId(@Param("memberId") Long memberId);
+
+}
